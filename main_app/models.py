521710ed0fded843ceeb97c354af276939e8c15e
dcheng <cheng.dorothy.s@gmail.com>
2021-05-21 14:42:16 -0600

    Created Hike model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Hike(models.Model):
+    name = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 50)
+    state = models.CharField(max_length = 2)
+    description = models.TextField()
+    length = models.FloatField()
+    elevation_gain = models.IntegerField()
+    ROUTE_TYPE_CHOICES = (
+        ('LP',  'Loop'),
+        ('OB', 'Out & back'),
+        ('PP', 'Point to point'),
+    )
+    route_type = models.CharField(max_length = 2, choices = ROUTE_TYPE_CHOICES)
+    DIFFICULT_CHOICES = (
+        ('E', 'Easy'),
+        ('M', 'Moderate'),
+        ('H', 'Hard'),
+    )
