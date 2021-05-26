94809c7ca5e150cc7bef0f5247dfcbc299813a19
dcheng <cheng.dorothy.s@gmail.com>
2021-05-26 11:04:58 -0600

    Updated filter to include min rating

--- main_app/filters.py
+++ main_app/filters.py
@@ -5,8 +5,9 @@ import django_filters
 
 class HikeFilter(django_filters.FilterSet):
     state = django_filters.CharFilter(lookup_expr='iexact')
-    length_gt = django_filters.NumberFilter(field_name='length', lookup_expr='gt', label='Min Length')
-    length_lt = django_filters.NumberFilter(field_name='length', lookup_expr='lt', label='Max Length')
+    rating_gte = django_filters.NumberFilter(field_name='average_rating', lookup_expr='gte', label = 'Min Rating')
+    length_gte = django_filters.NumberFilter(field_name='length', lookup_expr='gte', label='Min Length')
+    length_lte = django_filters.NumberFilter(field_name='length', lookup_expr='lte', label='Max Length')
     class Meta:
         model = Hike
         fields = ['state','route_type','difficulty']
