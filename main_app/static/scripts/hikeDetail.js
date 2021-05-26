3ff3a2684b4d91968f9338f2f4f4ae914b75c726
dcheng <cheng.dorothy.s@gmail.com>
2021-05-26 14:27:36 -0600

    Added photo carousel to hike detail page

--- main_app/static/scripts/hikeDetail.js
+++ main_app/static/scripts/hikeDetail.js
@@ -1,11 +1,33 @@
 $allPhotos = $('.detail-photo');
 
+function left() {
+    changePhoto(-1);
+};
+function right() {
+    changePhoto(1);
+};
+
+function changePhoto(direction) {
+    const currentIndex = Number($('.selected-img').attr('id').slice(5));
+    let nextIndex = direction + currentIndex;
+    if (nextIndex > $allPhotos.length - 1) {
+        nextIndex = 0;
+    } else if (nextIndex < 0) {
+        nextIndex = $allPhotos.length - 1;
+    }
+    $nextPhoto = $allPhotos.eq(nextIndex);
+    nextURL = $nextPhoto.attr('src');
+    $('.selected-img').attr('src', nextURL);
+    $('.selected-img').attr('id', `photo${nextIndex}`);
+};
+
 function displayPhoto(e) {
-    console.log('clicked');
     const $selectedPhoto = $(e.target);
+    const currentIndex = $selectedPhoto.index();
     const imageURL = $selectedPhoto.attr('src');
     const $photoModal = $('#photo-modal');
-    $('#selected-img').attr('src', imageURL);
+    $('.selected-img').attr('src', imageURL);
+    $('.selected-img').attr('id', `photo${currentIndex}`);
     $photoModal.show();
     window.addEventListener('click', (e) => {
         if (e.target == document.getElementById('modal-content')) {
@@ -14,4 +36,6 @@ function displayPhoto(e) {
     });
 };
 
-$allPhotos.on('click',displayPhoto);
+$allPhotos.on('click', displayPhoto);
+$('#left').on('click', left);
+$('#right').on('click', right);
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,10 @@ from .views import HikeCreateView, HikeUpdateView, HikeDeleteView, ReviewDeleteV
 app_name = 'hikes'
 urlpatterns = [
     path('', views.home, name='home'),
+    
     path('accounts/signup/', views.signup, name='signup'),
+
+    # Hikes paths
     path('hikes/add/', HikeCreateView.as_view(), name='add'),
     path('hikes/', views.index, name='index'),
     path('hikes/<pk>/update/', HikeUpdateView.as_view(), name='update'),
@@ -14,9 +17,13 @@ urlpatterns = [
     path('hikes/<int:hike_id>/favorite/', views.favorite, name='favorite'),
     path('hikes/<int:hike_id>/completed/', views.completed, name='completed'),
     path('hikes/<int:hike_id>/', views.detail, name='detail'),
+
+    # User paths
     path('user/<int:user_id>/', views.profile, name='profile'),
     path('user/<int:user_id>/update/', views.update_user, name='update_user'),
     path('user/<pk>/delete/', UserDeleteView.as_view(), name='delete_user'),
+
+    # Reviews paths
     path('reviews/<int:hike_id>/add/', views.add_review, name='add_review'),
     path('reviews/<int:review_id>/update/', views.update_review, name='update_review'),
     path('reviews/<pk>/delete/', ReviewDeleteView.as_view(), name='delete_review'),
