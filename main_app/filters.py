244b3abc035898410bc328f0ad010903780a7637
dcheng <cheng.dorothy.s@gmail.com>
2021-05-26 17:52:40 -0600

    Added hike name to index filter

--- main_app/filters.py
+++ main_app/filters.py
@@ -1,13 +1,13 @@
 # reference: https://simpleisbetterthancomplex.com/tutorial/2016/11/28/how-to-filter-querysets-dynamically.html
-# another reference: https://python.plainenglish.io/how-to-create-a-fancy-range-slider-filter-in-django-890bd0486e13
 from .models import Hike
 import django_filters
 
 class HikeFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
     state = django_filters.CharFilter(lookup_expr='iexact')
     rating_gte = django_filters.NumberFilter(field_name='average_rating', lookup_expr='gte', label = 'Min Rating')
     length_gte = django_filters.NumberFilter(field_name='length', lookup_expr='gte', label='Min Length')
     length_lte = django_filters.NumberFilter(field_name='length', lookup_expr='lte', label='Max Length')
     class Meta:
         model = Hike
-        fields = ['state','route_type','difficulty']
+        fields = ['name','state','route_type','difficulty']
