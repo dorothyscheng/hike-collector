cc0637dae8402fe0c097d51260f3cdbb98778bfe
dcheng <cheng.dorothy.s@gmail.com>
2021-05-26 14:58:59 -0600

    Updated profile page with sliding section menus

---
+++ main_app/static/scripts/profile.js
@@ -0,0 +1,24 @@
+function changeArrow($element) {
+    if ($element.hasClass('fa-chevron-down')) {
+        $element.removeClass('fa-chevron-down');
+        $element.addClass('fa-chevron-up');
+    } else {
+        $element.removeClass('fa-chevron-up');
+        $element.addClass('fa-chevron-down');
+    }
+}
+
+$('#favorites-nav').on('click',()=>{
+    $('#favorites-section').slideToggle();
+    changeArrow($('#favorites-arrow'));
+});
+
+$('#completed-nav').on('click',()=>{
+    $('#completed-section').slideToggle();
+    changeArrow($('#completed-arrow'));
+});
+
+$('#reviews-nav').on('click',()=>{
+    $('#reviews-section').slideToggle();
+    changeArrow($('#reviews-arrow'));
+});
